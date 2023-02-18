43c002416ab858f044b7c27a8cb544359efc0d42
hamdyadam97 <hamdyadam97@gmail.com>
2023-02-18 14:07:48 +0200

    key

--- src/page/login.js
+++ src/page/login.js
@@ -7,9 +7,10 @@ import TwitterLogin from "react-twitter-login";
 
 const EmailForm = () => {
  
-    const twitterApiKey = "cvtLOvPCCzczegnVb0BTvnNlC";
-    const twitterSecretKey = "mgGkeyZm6FQGR57WiRJsZt2fNHwN6vvqd3x0LdhLmibDrwltx2";
- 
+    const twitterApiKey = "F2dTYiwmvNfYbrgvfwyTUmuZo";
+    const twitterSecretKey = "Dr2Nc1dZFEnsJjw5BBKBwbqS2vk9rwp46mnXC3unPmajTXieic";
+    // ZFhuN0Q1eHhmNU1TdlFDODEwLVk6MTpjaQ
+    // E4MS5mJSH7eG5L-bIv6vBKOS9uCnT7JjpjRNq3POGNqNBVvh5q
     const twitterLogin = async (res, data) => {
         console.log(res)
     
